d865e73c7f150fe808bb2c9f99165267a9300e71
sanket622 <sanketkumar0068@gmail.com>
2025-06-16 17:26:04 +0530

    updates

--- projects/my-library/src/lib/my-button/my-button.ts
+++ projects/my-library/src/lib/my-button/my-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'lib-my-button',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './my-button.css'
 })
 export class MyButton {
+  @Input() data: any = { label: 'Hello World' };
+  @Output() dataChange = new EventEmitter<any>();
 
+  onClick() {
+    const modifiedData = { ...this.data, clicked: true };
+    this.dataChange.emit(modifiedData);
+  }
 }
