5b6c5f157dbbdc89e6b867488b816d8896687f79
sanket622 <sanketkumar0068@gmail.com>
2025-06-17 12:43:03 +0530

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyButton } from 'my-library';
-import {Practice} from './practice/practice'
+import { Practice } from './practice/practice'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyButton,Practice],
+  imports: [RouterOutlet, MyButton, Practice,CommonModule],
   template: `
     <router-outlet></router-outlet>
     <lib-my-button></lib-my-button>
-    <button>Add Card</button>
+    <button (click)="addCard()">Add Card</button>
+    <div *ngFor="let card of cards">
       <app-practice></app-practice>
+    </div>
   `,
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-workspace';
+  cards: number[] = [1];
+  
+  addCard() {
+    this.cards.push(this.cards.length + 1);
+  }
 }
--- src/app/practice/practice.ts
+++ src/app/practice/practice.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-practice',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './practice.html',
   styleUrl: './practice.css'
 })
